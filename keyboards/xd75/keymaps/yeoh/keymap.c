20dba04e582e08e4eed8edc9a77306fc317def2a
Chris Yeoh <christopher.e.yeoh@gmail.com>
2018-04-20 17:33:46 -0400

    Move shift to level 1. Move media keys around

--- keyboards/xd75/keymaps/yeoh/keymap.c
+++ keyboards/xd75/keymaps/yeoh/keymap.c
@@ -85,14 +85,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
  * |        |        |        |        |        |        |        | VOL-   |        |        | SF+C+[ | SF+C+] |        |        |        |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
- * |        |        |        |        |        |        | PREV   | PLAY   | NEXT   |  LEFT  | DOWN   |  UP    |  RIGHT |        |        |
+ * |        |        |        |        |        |        |        | MUTE   |        |  LEFT  | DOWN   |  UP    |  RIGHT |        |        |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
- * |        |        |        | CMD+[  | CMD+]  |        |        | MUTE   |        | CMD+LT | ALT+LF | ALT+RT | CMD+RT |        |        |
+ * | SHIFT  |        |        | CMD+[  | CMD+]  |        |        |        |        | CMD+LT | ALT+LF | ALT+RT | CMD+RT | CMD+/  |        |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
- * |RESET   |        |        | FN     |        |        |        |        |        |        |        |  FN    |        |        |        |
+ * |RESET   |        |        | FN     |        |        | PREV   | PLAY   | NEXT   |        |        |  FN    |        |        |        |
  * '--------------------------------------------------------------------------------------------------------------------------------------'
  */
- 
+
 //  [_FN] = { /* FUNCTION */
 //   { KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,  KC_F6,  KC_NLCK, KC_SLSH, KC_ASTR, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12 },
 //   { KC_MSEL, KC_CALC, KC_TRNS, KC_MAIL, KC_SPC, KC_SPC, KC_P7,   KC_P8,   KC_P9,   KC_MINS, KC_SPC,  KC_SPC,  KC_TRNS, KC_TRNS, KC_TRNS },
@@ -101,13 +101,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //   { RESET,   KC_TRNS, KC_TRNS, MO(_FN), KC_SPC, KC_SPC, KC_KP_0, KC_TRNS, KC_PDOT, KC_PENT, KC_TRNS, MO(_FN), KC_TRNS, KC_TRNS, KC_TRNS },
 //  }
 // };
- 
+
  [_FN] = { /* FUNCTION */
   { KC_NO,   KC_NO,   KC_NO,   KC_NO,             KC_NO,              KC_NO,  KC_NO,   KC_VOLU,   KC_NO,   KC_NO,     KC_NO,   KC_NO,   KC_NO,  KC_NO,  KC_NO },
   { KC_NO,   KC_NO,   KC_NO,   KC_NO,             KC_NO,              KC_NO,  KC_NO,   KC_VOLD,   KC_NO,   KC_NO,     LGUI(LSFT(KC_LBRACKET)), LGUI(LSFT(KC_RBRACKET)), KC_NO,  KC_NO,  KC_NO },
-  { KC_NO,   KC_NO,   KC_NO,   KC_NO,             KC_NO,              KC_NO,  KC_MPRV, KC_MPLY,   KC_MNXT, KC_LEFT,   KC_DOWN,   KC_UP,   KC_RGHT,  KC_NO,  KC_NO },
-  { KC_NO,   KC_NO,   KC_NO,   LGUI(KC_LBRACKET), LGUI(KC_RBRACKET),  KC_NO,  KC_NO,   KC_MUTE,   KC_NO,   LGUI(KC_LEFT), LALT(KC_LEFT), LALT(KC_RIGHT), LGUI(KC_RIGHT), LGUI(KC_SLASH),  KC_NO,  KC_NO },
-  { RESET,   KC_NO,   KC_NO,   MO(_FN),           KC_NO,              KC_NO,  KC_NO,   KC_NO,     KC_NO,   KC_NO,     KC_NO,   MO(_FN),   KC_NO,  KC_NO,  KC_NO },
+  { KC_NO,   KC_NO,   KC_NO,   KC_NO,             KC_NO,              KC_NO,  KC_NO,   KC_MUTE,   KC_NO,   KC_LEFT,   KC_DOWN,   KC_UP,   KC_RGHT,  KC_NO,  KC_NO },
+  { KC_LSFT, KC_NO,   KC_NO,   LGUI(KC_LBRACKET), LGUI(KC_RBRACKET),  KC_NO,  KC_NO,   KC_NO,     KC_NO,   LGUI(KC_LEFT), LALT(KC_LEFT), LALT(KC_RIGHT), LGUI(KC_RIGHT), LGUI(KC_SLASH),  KC_NO },
+  { RESET,   KC_NO,   KC_NO,   MO(_FN),           KC_NO,              KC_NO,  KC_MPRV, KC_MPLY,   KC_MNXT, KC_NO,     KC_NO,   MO(_FN),   KC_NO,  KC_NO,  KC_NO },
  }
 };
 
