cba08b39c9ab07fb2d2ffdea3c4af9c60dbff4d0
Chris Yeoh <christopher.e.yeoh@gmail.com>
2018-10-16 13:29:35 -0400

    Move volume keys up

--- keyboards/xd75/keymaps/yeoh/keymap.c
+++ keyboards/xd75/keymaps/yeoh/keymap.c
@@ -43,9 +43,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* YEOH
  * .--------------------------------------------------------------------------------------------------------------------------------------.
- * | `      | 1      | 2      | 3      | 4      | 5      | 6      |        | 7      | 8      | 9      | 0      | -      | =      | BACKSP |
+ * | `      | 1      | 2      | 3      | 4      | 5      | VOL-   | MUTE   | VOL+   | 8      | 9      | 0      | -      | =      | BACKSP |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------|
- * | TAB    | Q      | W      | E      | R      | T      | VOL-   | MUTE   | VOL+   | Y      | U      | I      | O      | P      | \      |
+ * | TAB    | Q      | W      | E      | R      | T      | 6      |        | 7      | Y      | U      | I      | O      | P      | \      |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------+--------|
  * | ESC/CTR| A      | S      | D      | F      | G      | [      | DEL    | ]      | H      | J      | K      | L      | ;      | '      |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------------------------+--------|
@@ -64,11 +64,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  // },
 
  [_QW] = { /* QWERTY */
-  { KC_GRV,           KC_1,    KC_2,    KC_3,    KC_4,   KC_5,   KC_6,    KC_NO,  KC_7,   KC_8,     KC_9,   KC_0,     KC_MINS, KC_EQL,  KC_BSPC },
-  { KC_TAB,           KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,   KC_VOLD, KC_MUTE, KC_VOLU, KC_Y,    KC_U,   KC_I,     KC_O,    KC_P,    KC_BSLS },
-  { CTL_T(KC_ESCAPE), KC_A,    KC_S,    KC_D,    KC_F,   KC_G,   KC_LBRC, KC_DEL,   KC_RBRC, KC_H,    KC_J,   KC_K,     KC_L,    KC_SCLN, KC_QUOT  },
-  { KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,   KC_MPRV, KC_MPLY,   KC_MNXT,   KC_N,    KC_M,   KC_COMM,  KC_DOT,  KC_SLSH, KC_RSFT },
-  { KC_LALT,          KC_MEH,  KC_NO,   MO(_FN), KC_LGUI, KC_ENT, KC_NO, KC_NO, KC_NO, KC_SPC,  KC_RGUI, MO(_FN), KC_NO,   KC_RCTL,   KC_RALT },
+  { KC_GRV,           KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   KC_VOLD, KC_MUTE,  KC_VOLU, KC_8,    KC_9,   KC_0,     KC_MINS, KC_EQL,  KC_BSPC },
+  { KC_TAB,           KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,   KC_6,    KC_NO,    KC_7,    KC_Y,    KC_U,   KC_I,     KC_O,    KC_P,    KC_BSLS },
+  { CTL_T(KC_ESCAPE), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,   KC_LBRC, KC_DEL,   KC_RBRC, KC_H,    KC_J,   KC_K,     KC_L,    KC_SCLN, KC_QUOT  },
+  { KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   KC_MPRV, KC_MPLY,  KC_MNXT, KC_N,    KC_M,   KC_COMM,  KC_DOT,  KC_SLSH, KC_RSFT },
+  { KC_LALT,          KC_MEH,  KC_NO,   MO(_FN), KC_LGUI, KC_ENT, KC_NO,   KC_NO,    KC_NO,   KC_SPC,  KC_RGUI, MO(_FN), KC_NO,   KC_RCTL, KC_RALT },
  },
 
 /* FUNCTION
