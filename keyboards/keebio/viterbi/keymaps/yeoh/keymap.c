3c1b4bb93be53523dad13a67c72ff2810fdfe93a
Chris Yeoh <christopher.e.yeoh@gmail.com>
2019-05-03 12:25:45 -0400

    Add music keys to raised layer.  Move ctrl from dual key.

--- keyboards/keebio/viterbi/keymaps/yeoh/keymap.c
+++ keyboards/keebio/viterbi/keymaps/yeoh/keymap.c
@@ -25,15 +25,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* QWERTY
  * ,------------------------------------------------.      ,------------------------------------------------.
- * |  Ins |   `  |   1  |   2  |   3  |   4  |   5  |      |   6  |   7  |   8  |   9  |   0  | Bksp | Del  |
+ * |    `  |   1  |   2  |   3  |   4  |   5  |  6  |      |   7  |   8  |   9  |   0  |   -  |   =  | Bksp |
  * |------+------+------+------+------+------+------|      |------+------+------+------+------+------+------|
- * |   -  | Tab  |   Q  |   W  |   E  |   R  |   T  |      |   Y  |   U  |   I  |   O  |   P  |   [  |   ]  |
+ * |  Tab  |   Q  |   W  |   E  |   R  |   T  | Vol+|      |      |   Y  |   U  |   I  |   O  |   P  |   \  |
  * |------+------+------+------+------+------+------|      |------+------+------+------+------+------+------|
- * |   =  | Esc  |   A  |   S  |   D  |   F  |   G  |      |   H  |   J  |   K  |   L  |   ;  |   "  |Enter |
+ * |  Esc  |   A  |   S  |   D  |   F  |   G  | {   |      |      |   H  |   J  |   K  |   L  |   ;  |   '  |
  * |------+------+------+------+------+------+------|      |------+------+------+------+------+------+------|
- * | Pg Up| Shift|   Z  |   X  |   C  |   V  |   B  |      |   N  |   M  |   ,  |   .  |   /  | Home | End  |
+ * |  Shift|   Z  |   X  |   C  |   V  |   B  | [   |      |      |   N  |   M  |   ,  |   .  |   /  | Shift|
  * |------+------+------+------+------+------+------|      |------+------+------+------+------+------+------|
- * | Pg Dn|Adjust| Ctrl | Alt  | GUI  |Lower |Space |      |Space |Raise | Left | Down |  Up  | Right|   \  |
+ * | Adjust| Ctrl | Alt  | GUI  |Lower |Space |     |      |      |Space |Raise | Left | Down |  Up  | Right|
  * `------------------------------------------------'      `------------------------------------------------'
  */
   // [_QWERTY] = LAYOUT_ortho_5x14(
@@ -48,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB          , KC_Q   , KC_W   , KC_E      , KC_R   , KC_T   , KC_VOLD,         KC_VOLU, KC_Y   , KC_U   , KC_I      , KC_O   , KC_P   , KC_BSLS,
     CTL_T(KC_ESCAPE), KC_A   , KC_S   , KC_D      , KC_F   , KC_G   , KC_LCBR,         KC_RBRC, KC_H   , KC_J   , KC_K      , KC_L   , KC_SCLN, KC_QUOT,
     KC_LSFT         , KC_Z   , KC_X   , KC_C      , KC_V   , KC_B   , KC_LBRC,         KC_RCBR, KC_N   , KC_M   , KC_COMM   , KC_DOT , KC_SLSH, KC_RSFT,
-    KC_LALT         , KC_MEH , KC_NO  , MO(_LOWER), KC_LGUI, KC_NO  , KC_ENT ,         KC_SPC , KC_NO  , KC_RGUI, MO(_RAISE), KC_NO  , KC_NO  , KC_NO
+    KC_LALT         , KC_MEH , KC_NO  , MO(_LOWER), KC_LGUI, KC_NO  , KC_ENT ,         KC_SPC , KC_NO  , KC_RGUI, MO(_RAISE), KC_NO  , KC_NO  , KC_RCTL
   ),
 
 /* Lower
@@ -87,7 +87,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------|      |------+------+------+------+------+------+------|
  * |   +  |      |  F1  |  F2  |  F3  |  F4  |  F5  |      |  F6  |   -  |   =  |   [  |   ]  |      |      |
  * |------+------+------+------+------+------+------|      |------+------+------+------+------+------+------|
- * |      |      |  F7  |  F8  |  F9  |  F10 |  F11 |      |  F12 |ISO # |ISO / |      |      |      |      |
+ * |      |      |  F7  |  F8  |  F9  |  F10 |  F11 |      |  F12 |ISO # |ISO / | Prev | Next | Play |      |
  * |------+------+------+------+------+------+------|      |------+------+------+------+------+------+------|
  * |      |      |      |      |      |      |      |      |      |      | Next | Vol- | Vol+ | Play |      |
  * `------------------------------------------------'      `------------------------------------------------'
@@ -96,8 +96,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, KC_TILD, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  ,     KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 ,
     KC_UNDS, _______, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,     KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_LCBR, KC_RCBR,
     KC_PLUS, _______, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  ,     KC_F6  , KC_MINS, KC_EQL , KC_LBRC, KC_RBRC, _______, _______,
-    _______, _______, KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 ,     KC_F12 , KC_NUHS, KC_NUBS, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______,     _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY, KC_MUTE
+    _______, _______, KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 ,     KC_F12 , KC_NUHS, KC_NUBS, KC_MPRV, KC_MNXT, KC_MPLY, KC_MUTE,
+    _______, _______, _______, _______, _______, _______, _______,     _______, _______, KC_MNXT, MO(_RAISE), KC_NO  , KC_NO  , KC_RCTL
   ),
 
   [_ADJUST] = LAYOUT_ortho_5x14(
