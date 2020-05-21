e79e6a58f874fc281bc62ccae7536d7644eb5d5a
Chris Yeoh <christopher.e.yeoh@gmail.com>
2020-05-21 16:38:12 -0400

    Fix raise to media keys

--- keyboards/keebio/viterbi/keymaps/yeoh/keymap.c
+++ keyboards/keebio/viterbi/keymaps/yeoh/keymap.c
@@ -27,7 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT_ortho_5x14(
     KC_GRV          , KC_1   , KC_2   , KC_3      , KC_4   , KC_5   , KC_6   ,         KC_7   , KC_8   , KC_9   , KC_0      , KC_MINS, KC_EQL , KC_BSPC,
     KC_TAB          , KC_Q   , KC_W   , KC_E      , KC_R   , KC_T   , KC_VOLD,         KC_VOLU, KC_Y   , KC_U   , KC_I      , KC_O   , KC_P   , KC_BSLS,
-    CTL_T(KC_ESCAPE), KC_A   , KC_S   , KC_D      , KC_F   , KC_G   , KC_LCBR,         KC_RCBR, KC_H   , KC_J   , KC_K      , KC_L   , KC_SCLN, KC_QUOT,
+    CTL_T(KC_ESCAPE), KC_A   , KC_S   , KC_D      , KC_F   , KC_G   , KC_LCBR,         KC_RCBR, KC_H   , KC_J   , KC_K      , KC_L   , KC_SCLN, CTL_T(KC_QUOT),
     KC_LSFT         , KC_Z   , KC_X   , KC_C      , KC_V   , KC_B   , KC_LBRC,         KC_RBRC, KC_N   , KC_M   , KC_COMM   , KC_DOT , KC_SLSH, KC_RSFT,
     KC_LALT         , KC_MEH , KC_NO  , MO(_LOWER), KC_LGUI, KC_NO  , KC_ENT ,         KC_SPC , KC_NO  , KC_RGUI, MO(_RAISE), KC_NO  , KC_NO  , KC_LCTL
   ),
@@ -37,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     RGB_VAI, RGB_VAD,      KC_NO,        KC_NO,             KC_NO,              KC_NO,  KC_NO,   KC_NO,   KC_NO,         LGUI(LSFT(KC_LBRACKET)), LGUI(LSFT(KC_RBRACKET)), KC_NO,          KC_NO,           KC_NO,
     KC_NO,   KC_NO,        KC_NO,        LGUI(KC_LBRACKET), LGUI(KC_RBRACKET),  KC_NO,  KC_NO,   KC_NO,   KC_LEFT,       KC_DOWN,                 KC_UP,                   KC_RGHT,        KC_NO,           KC_NO,
     KC_LSFT, LGUI(KC_Z),   LGUI(KC_X),   LGUI(KC_C),        LGUI(KC_V),         KC_NO,  KC_NO,   KC_NO,   LGUI(KC_LEFT), LALT(KC_LEFT),           LALT(KC_RIGHT),          LGUI(KC_RIGHT), LGUI(KC_SLASH),  KC_NO,
-    RESET,   KC_NO,        KC_NO,        MO(_LOWER),        KC_NO,              KC_ENT, KC_MPRV, KC_MNXT, KC_SPC,        KC_NO,                   MO(_LOWER),              KC_NO,          KC_NO,           KC_NO
+    RESET,   KC_NO,        KC_NO,        MO(_LOWER),        KC_NO,              KC_ENT, KC_MPRV, KC_MNXT, KC_SPC,        KC_NO,                   MO(_RAISE),              KC_NO,          KC_NO,           KC_NO
   ),
 
   [_RAISE] = LAYOUT_ortho_5x14(
